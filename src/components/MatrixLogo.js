9a50a0871d4b796f0974c55d76aca7d2f8882936
Nikita Chavda <home@expertwise.local>
2025-03-16 18:08:56 +0530

    header logo updates

--- src/components/MatrixLogo.js
+++ src/components/MatrixLogo.js
@@ -1,26 +1,13 @@
 "use client";
+import styles from '../styles/logo.module.css';
 
-import { useState, useEffect } from "react";
-import Image from "next/image";
 
 const MatrixLogo = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth <= 768);
-    handleResize(); // Set on mount
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
-    <Image
-      src={isMobile ? "/matrix-pharma-logo.png" : "/matrix-pharma.svg"}
-      alt="Matrix Pharma Logo"
-      width={isMobile ? 211 : 280}
-      height={isMobile ? 40 : 60}
-      priority
-    />
+    <>
+    <img className={styles.logoweb} src="/matrix-pharma.svg"></img>
+    <img className={styles.logomob} src="/matrix-pharma-logo.png"></img>
+    </>
   );
 };
 
